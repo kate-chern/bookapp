f02fd1f25b5dbfc7b685a5f7b093705deb8965f4
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-08-13 12:25:09 +0300

    first commit

---
+++ src/main/java/project/bokapp/BokappApplication.java
@@ -0,0 +1,13 @@
+package project.bokapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokappApplication.class, args);
+	}
+
+}
