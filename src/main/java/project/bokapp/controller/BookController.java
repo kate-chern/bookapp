a592595261a8df227569a79de7cc4684214c2b64
Kate Chernyavska <ccarts.kpi@gmail.com>
2023-09-03 05:31:06 +0300

    added new methods as update, delete and search

--- src/main/java/project/bokapp/controller/BookController.java
+++ src/main/java/project/bokapp/controller/BookController.java
@@ -2,13 +2,18 @@ package project.bokapp.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import project.bokapp.dto.BookDto;
+import project.bokapp.dto.BookSearchParametersDto;
 import project.bokapp.dto.CreateBookRequestDto;
 import project.bokapp.service.BookService;
 
@@ -31,5 +36,22 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto findById(@PathVariable Long id) {
         return bookService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto update(@PathVariable Long id,
+                          @RequestBody CreateBookRequestDto createBookRequestDto) {
+        return bookService.update(id, createBookRequestDto);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> search(BookSearchParametersDto bookSearchParametersDto) {
+        return bookService.search(bookSearchParametersDto);
     }
 }
---
+++ src/main/java/project/bokapp/repository/BookSpecificationProviderManager.java
@@ -0,0 +1,23 @@
+package project.bokapp.repository;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import project.bokapp.exception.EntityNotFoundException;
+import project.bokapp.model.Book;
+
+@RequiredArgsConstructor
+@Component
+public class BookSpecificationProviderManager implements SpecificationProviderManager<Book> {
+    private final List<SpecificationProvider<Book>> bookSpecificationProviders;
+
+    @Override
+    public SpecificationProvider<Book> getSpecificationProvider(String key) {
+        return bookSpecificationProviders.stream()
+                .filter(p -> p.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Can't find correct specification provider for key " + key));
+    }
+}
+
